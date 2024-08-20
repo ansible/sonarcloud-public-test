ca0e2a924426cca1299c71b72bd205c4d4a1c734
Matt Clay <matt@mystile.com>
2024-08-20 11:08:39 -0700

    Try to trigger more rules

--- test/plain_exception.py
+++ test/plain_exception.py
@@ -1,1 +1,11 @@
+# FIXME: this is a FIXME
+password = 'super secret'
+
+def do_stuff(pwd: str) -> None:
+    print(f'My password is: {pwd}')
+
+
+do_stuff(password)
+
+# TODO: this is a TODO
 raise Exception()
