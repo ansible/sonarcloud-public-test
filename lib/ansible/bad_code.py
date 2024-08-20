7c016e78ec9d2b692f404e271b69677953e9c190
Matt Clay <matt@mystile.com>
2024-08-20 11:51:35 -0700

    Bad code

---
+++ lib/ansible/bad_code.py
@@ -0,0 +1,14 @@
+# FIXME: this is a FIXME
+password = 'super secret'
+
+def do_stuff(pwd: str) -> None:
+    print(f'My password is: {pwd}')
+
+
+do_stuff(password)
+
+assert tuple()
+assert (1, 2)
+
+# TODO: this is a TODO
+raise Exception()
